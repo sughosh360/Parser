36d2154e21ee5473def72b1c6af5233049c6437c
Sughosh V Kauhsik <sughosh.kaushik360@gmail.com>
2016-02-10 19:36:28 +0530

    Update Integer_Parser.java

--- Integer_Parser.java
+++ Integer_Parser.java
@@ -3,6 +3,21 @@ import java.lang.*;
 import java.io.*;
 
 class Integer_Parser{
-
+      
+        public stringToInt(String s){
+          boolean isNegative = false;
+          if(s.charAt(0) == '-'){
+            isNegative = true;
+          }
+          int answer = 0;
+          for(int i = 0;i<s.length();i++){
+            answer += s.charAt(i) - '0';
+            answer *= 10;
+          }
+          if(isNegative){
+            answer *= -1;
+          }
+          return answer;
+        }
 
 }
